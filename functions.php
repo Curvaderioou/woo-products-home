9c5854e8646c3e2570047a5d14e14c000c7589d9
Curvaderioou <felipedegenerportella@yahoo.com>
2021-10-15 23:35:25 -0300

    page functions e page-home para e-commerce woo

---
+++ functions.php
@@ -0,0 +1,54 @@
+<?php
+
+function handel_add_woocommerce_suport(){
+  add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'handel_add_woocommerce_suport');
+
+
+/*
+function handel_css(){
+  wp_register_style('handel-style', get_template_directory_uri() . '/style.css')
+}
+
+echo get_template_directory_uri();
+*/
+function handel_css(){
+  wp_register_style('handel-style', get_template_directory_uri() . '/style.css', [], '1.0.0');
+  wp_enqueue_style('handel-style');
+}
+add_action('wp_enqueue_scripts', 'handel_css');
+
+function handel_custom_images(){
+  add_image_size('slide', 1000, 800, ['center', 'top']);
+  update_option('medium_crop', 1);
+}
+add_action('after_setup_theme', 'handel_custom_images');
+
+
+function handel_loop_shop_per_page(){
+  return 4;
+}
+
+add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
+
+function handel_product_list($products) { ?>
+  <ul class="products-list">
+  <?php foreach($products as $product) { ?>
+  <li class="product-item">
+    <a href="<?= $product['link']; ?>">
+      <div class="product-info">
+        <img src="<?= $product['img']; ?>" alt="<?= $product['name'];
+        ?>">
+        <h2><?= $product['name']; ?> <span><?= $product['preco']; ?></span></h2>
+      </div>
+      <div class="product-overlay">
+        <span>Ver Mais</span>  
+      </div>
+    </a>
+  </li>
+  <?php } ?>
+  </ul>
+<?php
+  }
+?>
---
+++ page-home.php
@@ -0,0 +1,83 @@
+<?php 
+// Template name: Home
+get_header(); ?>
+
+  <?php
+  
+  $products_slide = wc_get_products([
+    'limit' => 6,
+    'tag' => ['slide'],
+  ]);
+
+  $products_new = wc_get_products([
+      'limit' => 9,
+      'orderby' => 'date',
+      'order' => 'DESC',
+  ]);
+
+  $products_sales = wc_get_products([
+      'limit' => 9,
+      'meta_key' => 'total_sales',
+      'orderby' => 'meta_value_num',
+      'order' => 'DESC',
+  ]);
+
+
+
+  function format_products($products, $img_size){
+    $products_final = [];
+    foreach($products as $product) {
+      $products_final[] = [
+        'name' => $product->get_name(),
+        'preco' => $product->get_price_html(),
+        'link' => $product->get_permalink(),
+        'img' => wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
+      ];
+    }
+    return $products_final;
+  }
+
+  $data = [];
+
+  $data['slide'] = format_products($products_slide, 'slide');
+  $data['lancamentos'] = format_products($products_new, 'medium');
+  $data['vendas'] = format_products($products_sales, 'medium' );
+
+  ?>
+
+  <?php if(have_posts()) { while (have_posts()) { the_post(); ?>
+
+    <ul class="vantagens">
+      <li>Frete Grátis</li>
+      <li>Troca Fácil</li>
+      <li>Em até 12x</li>
+    </ul>
+
+    <section class="slide-wrapper">
+      <ul class="slide">
+          <?php foreach($data['slide'] as $product) { ?>
+          <li class="slide-item"> 
+            <img src="<?= $product['img']; ?>" alt="<?= $product['name']; ?>">
+            <div class="slide-info">
+              <span class="slide-preco"><?= $product['preco']; ?></span>
+              <h2 class="slide-nome"><?= $product['name']; ?></h2>
+              <a class="slide-link" href="<?= $product['link']; ?>">Ver Produto</a>
+            </div>
+          </li>
+          <?php } ?>
+      </ul>
+    </section>
+
+    <section class="container">
+        <h1 class="subtitulo">Lançamentos</h1>
+        <?php handel_product_list($data['lancamentos']); ?>
+    </section>
+
+    <section class="container">
+        <h1 class="subtitulo">Mais vendidos</h1>
+        <?php handel_product_list($data['vendas']); ?>
+    </section>
+
+  <?php }} ?>
+
+<?php get_footer(); ?>
