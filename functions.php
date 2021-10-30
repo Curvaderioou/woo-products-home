ea4a28e0dbcea33c9b41331c8db25bb2b302cffe
Curvaderioou <felipedegenerportella@yahoo.com>
2021-10-30 16:27:01 -0300

    add apos final do projeto e do estudo

--- functions.php
+++ functions.php
@@ -27,28 +27,52 @@ add_action('after_setup_theme', 'handel_custom_images');
 
 
 function handel_loop_shop_per_page(){
-  return 4;
+  return 6;
 }
 
 add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
 
-function handel_product_list($products) { ?>
-  <ul class="products-list">
-  <?php foreach($products as $product) { ?>
-  <li class="product-item">
-    <a href="<?= $product['link']; ?>">
-      <div class="product-info">
-        <img src="<?= $product['img']; ?>" alt="<?= $product['name'];
-        ?>">
-        <h2><?= $product['name']; ?> <span><?= $product['preco']; ?></span></h2>
-      </div>
-      <div class="product-overlay">
-        <span>Ver Mais</span>  
-      </div>
-    </a>
-  </li>
-  <?php } ?>
+include(get_template_directory() . '/inc/product-list.php');
+
+function remove_some_body_class($classes){
+ $woo_class = array_search('woocommerce', $classes);
+ $woopage_class = array_search('woocommerce-page', $classes);
+ $search = in_array('archive', $classes) || in_array('product-template-default', $classes);
+ if($woo_class && $woopage_class && $search){
+   unset($classes[$woo_class]);
+   unset($classes[$woopage_class]);
+ }
+ return $classes;
+}
+
+add_filter('body_class', 'remove_some_body_class');
+
+  include(get_template_directory() . '/inc/user-custom-menu.php');
+  
+  include(get_template_directory() . '/inc/checkout-customizado.php');
+
+/* O hook abaixo inclui algo no top do e-mail
+function handel_change_email_header(){
+  echo '<h2 style="text-align: center" >Mensagem Header</h2>';
+}
+add_action('woocommerce_email_header', 'handel_change_email_header');
+*/
+
+function handel_change_email_footer_text($text){
+  echo 'Handel
+  
+  <ul style="padding: 0px; margin: 0px; list-style: none;">
+    <li><a href="/">Facebook</a></li>
+    <li><a href="/">Instagram</a></li>
+    <li><a href="/">Twitter</a></li>
   </ul>
-<?php
-  }
+  ';
+}
+
+add_filter('woocommerce_email_footer_text', 'handel_change_email_footer_text');
+
+//abaixo conseguimos incluir tambem no email, se é para presente e se tem mensagem.
+
+
 ?>
+
--- page-home.php
+++ page-home.php
@@ -7,41 +7,48 @@ get_header(); ?>
   $products_slide = wc_get_products([
     'limit' => 6,
     'tag' => ['slide'],
+    'stock_status' => 'instock',
   ]);
 
   $products_new = wc_get_products([
-      'limit' => 9,
+      'limit' => 6,
       'orderby' => 'date',
       'order' => 'DESC',
+      'stock_status' => 'instock',
   ]);
 
   $products_sales = wc_get_products([
-      'limit' => 9,
+      'limit' => 6,
       'meta_key' => 'total_sales',
       'orderby' => 'meta_value_num',
       'order' => 'DESC',
+      'stock_status' => 'instock',
   ]);
-
-
-
-  function format_products($products, $img_size){
-    $products_final = [];
-    foreach($products as $product) {
-      $products_final[] = [
-        'name' => $product->get_name(),
-        'preco' => $product->get_price_html(),
-        'link' => $product->get_permalink(),
-        'img' => wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
-      ];
-    }
-    return $products_final;
-  }
 
   $data = [];
 
   $data['slide'] = format_products($products_slide, 'slide');
   $data['lancamentos'] = format_products($products_new, 'medium');
   $data['vendas'] = format_products($products_sales, 'medium' );
+
+  $home_id = get_the_ID();
+  $categoria_esquerda = get_post_meta($home_id, 'categoria_esquerda', true);
+  $categoria_direita = get_post_meta($home_id, 'categoria_direita', true);
+
+  function get_product_category_data($category){
+    $cat = get_term_by('slug', $category, 'product_cat');
+    $cat_id = $cat->term_id;
+    $img_id = get_term_meta($cat_id, 'thumbnail_id', true);
+    return [
+      'name' => $cat->name,
+      'id' => $cat_id,
+      'link' => get_term_link($cat_id, 'product_cat'),
+      'img' => wp_get_attachment_image_src($img_id, 'slide')[0]
+    ];
+  }
+
+  $data['categorias'][$categoria_esquerda] = get_product_category_data($categoria_esquerda);
+  $data['categorias'][$categoria_direita] = get_product_category_data($categoria_direita);
 
   ?>
 
@@ -61,7 +68,7 @@ get_header(); ?>
             <div class="slide-info">
               <span class="slide-preco"><?= $product['preco']; ?></span>
               <h2 class="slide-nome"><?= $product['name']; ?></h2>
-              <a class="slide-link" href="<?= $product['link']; ?>">Ver Produto</a>
+              <a class="btn-link" href="<?= $product['link']; ?>">Ver Produto</a>
             </div>
           </li>
           <?php } ?>
@@ -72,6 +79,15 @@ get_header(); ?>
         <h1 class="subtitulo">Lançamentos</h1>
         <?php handel_product_list($data['lancamentos']); ?>
     </section>
+
+    <section class="categorias-home">
+    <?php foreach($data['categorias'] as $categoria) { ?>
+        <a href="<?= $categoria['link']?>">
+          <img src="<?= $categoria['img']?>" alt="<?= $categoria['name']?>">
+          <span class="btn-link"><?= $categoria['name'] ?></span>
+        </a>
+    <?php } ?>
+  </section>
 
     <section class="container">
         <h1 class="subtitulo">Mais vendidos</h1>
